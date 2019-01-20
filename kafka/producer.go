9c9c762533a4fe4fe608bb3a8e946325a12ff504
yudong <yudong@asiainfo.com>
2019-01-21 00:12:30 +0800

    h

--- kafka/producer.go
+++ kafka/producer.go
@@ -1,47 +1,63 @@
 package kafka
 
 import (
-	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/Shopify/sarama"
 )
 
-// Producer is interface for sending messages to Kafka.
-type Producer interface {
-	SendMessage(topic string, message interface{}) error
-}
+func AsyncProducer(mq []string) {
 
-type producer struct {
-	Producer sarama.SyncProducer
-}
+	config := sarama.NewConfig()
+	//等待服务器所有副本都保存成功后的响应
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	//随机向partition发送消息
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	//是否等待成功和失败后的响应,只有上面的RequireAcks设置不是NoReponse这里才有用.
+	config.Producer.Return.Successes = true
+	config.Producer.Return.Errors = true
+	//设置使用的kafka版本,如果低于V0_10_0_0版本,消息中的timestrap没有作用.需要消费和生产同时配置
+	//注意，版本设置不对的话，kafka会返回很奇怪的错误，并且无法成功发送消息
+	config.Version = sarama.V0_10_0_1
 
-// NewProducer returns a new SyncProducer for give brokers addresses.
-func NewProducer(broker string) (Producer, error) {
-	kafkaConfig := sarama.NewConfig()
-	kafkaConfig.Producer.Return.Successes = true
-
-	kafkaProducer, err := sarama.NewSyncProducer([]string{broker}, kafkaConfig)
-	if err != nil {
-		return nil, err
+	fmt.Println("start make producer")
+	//使用配置,新建一个异步生产者
+	producer, e := sarama.NewAsyncProducer([]string{"127.0.0.1:6667"}, config)
+	if e != nil {
+		fmt.Println(e)
+		return
 	}
+	defer producer.AsyncClose()
 
-	return &producer{Producer: kafkaProducer}, nil
-}
+	//循环判断哪个通道发送过来数据.
+	fmt.Println("start goroutine")
+	go func(p sarama.AsyncProducer) {
+		for {
+			select {
+			case <-p.Successes():
+				//fmt.Println("offset: ", suc.Offset, "timestamp: ", suc.Timestamp.String(), "partitions: ", suc.Partition)
+			case fail := <-p.Errors():
+				fmt.Println("err: ", fail.Err)
+			}
+		}
+	}(producer)
 
-func (p *producer) SendMessage(topic string, message interface{}) error {
-	msg, err := json.Marshal(message)
-	if err != nil {
-		return fmt.Errorf("cannot marshal message %v: %v", message, err)
+	var value string
+	for i := 0; i < len(mq); i++ {
+		value = mq[i]
+
+		// 发送的消息,主题。
+		// 注意：这里的msg必须得是新构建的变量，不然你会发现发送过去的消息内容都是一样的，因为批次发送消息的关系。
+		msg := &sarama.ProducerMessage{
+			Topic: "testNJ",
+		}
+
+		//将字符串转化为字节数组
+		msg.Value = sarama.ByteEncoder(value)
+		//fmt.Println(value)
+
+		//使用通道发送
+		producer.Input() <- msg
 	}
-
-	_, _, err = p.Producer.SendMessage(&sarama.ProducerMessage{
-		Topic: topic,
-		Value: sarama.ByteEncoder(msg),
-	})
-	if err != nil {
-		return fmt.Errorf("cannot send message %v: %v", message, err)
-	}
-
-	return nil
 }
